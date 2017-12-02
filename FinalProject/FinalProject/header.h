74cfdff5c2d5cd382c2046e9c6da12f1e77d350f
Kellers176 <34107413+Kellers176@users.noreply.github.com>
2017-12-02 14:01:58 -0500

    Update

--- FinalProject/FinalProject/header.h
+++ FinalProject/FinalProject/header.h
@@ -1,7 +1,18 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+#include <iostream>
+#include <string>
 #include "Association.h"
+#include "Itemset.h"
+#include "ShoppingCart.h"
+#include "Store.h"
+
+
+	void Apriori();
+	void createShoppingCarts();
+
+
 
 
 #endif // !HEADER_H
