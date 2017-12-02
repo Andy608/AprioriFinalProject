367133ad647532e36e3fd2d5ee36b114ad46dce5
Kellers176 <34107413+Kellers176@users.noreply.github.com>
2017-12-02 13:40:02 -0500

    Updated Project
    Fixed Hopefully

---
+++ FinalProject/FinalProject/Itemset.h
@@ -0,0 +1,18 @@
+#ifndef ITEMSET_H
+#define ITEMSET_H
+
+class Itemset
+{
+private:
+	int mSizeOfItemset;
+	int *mItems;
+
+public:
+	Itemset();
+	~Itemset();
+
+	void createItemSet(int *items, int length);
+};
+
+
+#endif // !ITEMSET_H
